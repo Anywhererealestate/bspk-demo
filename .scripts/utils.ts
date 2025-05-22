b832019089e938f17ee0f0e357954671af9d7114
anybrian <brian.reed@anywhere.re>
2025-05-21 20:54:37 -0400

    fix getUIRoot

--- .scripts/utils.ts
+++ .scripts/utils.ts
@@ -3,6 +3,8 @@ import path from 'path';
 
 export function getUIRoot() {
     const pkgs = execSync('npm ls --depth=0', { encoding: 'utf-8' }).toString();
-    const linkedPath = pkgs.match(/@bspk\/ui@.* -> (.*)/)?.[1];
-    return path.resolve(__dirname, '../', linkedPath?.replace('src', '') || 'node_modules/@bspk/ui');
+    const linkedPathToUI = pkgs.match(/@bspk\/ui@.* -> (.*)/)?.[1];
+    return path
+        .resolve(__dirname, '../', linkedPathToUI?.replace('src', '') || 'node_modules/@bspk/ui')
+        .replace('bspk-demo/bspk-demo', 'bspk-demo');
 }
