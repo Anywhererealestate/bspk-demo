900b33c4fd2eff820a1b3295ddcb39cfffc5485c
anybrian <brian.reed@anywhere.re>
2025-06-12 02:15:31 -0400

    get proper ui hash

--- .scripts/create-meta.ts
+++ .scripts/create-meta.ts
@@ -13,29 +13,31 @@ const isLocalDev = execSync('npm ls @bspk/ui', { encoding: 'utf-8' }).trim().inc
 if (fs.existsSync(localUIPath) && isLocalDev) {
     const fileUpdated = process.argv[2] ? ` -- update=${process.argv[2]}` : '';
 
-    execSync(
-        `export DEV_GIT_TOKEN=local && cd ${localUIPath} && npm run meta hash=local out=${demoRoot} ${fileUpdated}`,
-        {
-            stdio: 'inherit',
-        },
-    );
+    execSync(`cd ${localUIPath} && npm run meta hash=local out=${demoRoot} ${fileUpdated}`, {
+        stdio: 'inherit',
+    });
 
     if (!fileUpdated) execSync(`npx eslint --fix ${demoRoot}/meta.ts`, { stdio: 'inherit' });
-} else {
-    const DEV_GIT_TOKEN = process.env.DEV_GIT_TOKEN;
-
-    if (DEV_GIT_TOKEN) {
-        console.log('Installing dev branch of bspk-ui package...\n\n');
-
-        execSync(`npm install https://${DEV_GIT_TOKEN}@github.com/Anywhererealestate/bspk-ui#dev`, {
-            stdio: 'inherit',
-        });
-    } else {
-        console.log('DEV_GIT_TOKEN not set, skipping dev package installation.');
-    }
-    const hash = execSync('npm explore @bspk/ui -- git rev-parse --short HEAD', { encoding: 'utf-8' }).trim();
-
-    execSync(`npm explore @bspk/ui -- npm run meta out=${demoRoot} hash=${hash}`, { stdio: 'inherit' });
-
-    execSync(`npx eslint --fix ${demoRoot}/meta.ts`, { stdio: 'inherit' });
+    process.exit(0);
 }
+
+// NOT LOCAL DEV MODE
+
+const DEV_GIT_TOKEN = process.env.DEV_GIT_TOKEN;
+
+if (DEV_GIT_TOKEN) {
+    console.log('Installing dev branch of bspk-ui package...\n\n');
+
+    execSync(`npm install https://${DEV_GIT_TOKEN}@github.com/Anywhererealestate/bspk-ui#dev`, {
+        stdio: 'inherit',
+    });
+} else {
+    console.log('DEV_GIT_TOKEN not set, skipping dev package installation.');
+}
+
+// Get the current commit hash of the @bspk/ui package
+const hash = execSync('npm list @bspk/ui', { encoding: 'utf-8' }).trim().split('#')[1].substring(0, 7);
+
+execSync(`npm explore @bspk/ui -- npm run meta out=${demoRoot} hash=${hash}`, { stdio: 'inherit' });
+
+execSync(`npx eslint --fix ${demoRoot}/meta.ts`, { stdio: 'inherit' });
