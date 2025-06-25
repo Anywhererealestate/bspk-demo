b3e7a9924fcdb44303f42d2b8b2a40f792000d56
anybrian <brian.reed@anywhere.re>
2025-06-25 11:38:16 -0400

    fix axe results, add new ui provider

--- src/utils/globalState.tsx
+++ src/utils/globalState.tsx
@@ -1,37 +1,35 @@
 import { Brand } from '@bspk/ui';
+import { useUIContext } from '@bspk/ui/hooks/useUIContext';
+import { ColorTheme } from '@bspk/ui/utils/uiContext';
 import { createContext, useContext } from 'react';
 
-export const COLOR_THEMES = ['light', 'dark'] as const;
-
-export type ColorTheme = (typeof COLOR_THEMES)[number];
-
 export const globalStateDefault: GlobalStateContext = {
-    theme: 'light',
     brand: 'anywhere',
     showTouchTarget: false,
     setBrand: () => {},
-    setTheme: () => {},
     setShowTouchTarget: () => {},
     resetGlobalState: () => {},
 } as const;
 
 export type GlobalState = {
     brand: Brand;
-    theme: ColorTheme;
     showTouchTarget?: boolean;
 };
 
 export type GlobalStateContext = GlobalState & {
     setBrand: (brand: Brand) => void;
-    setTheme: (theme: ColorTheme) => void;
     setShowTouchTarget: (showTouchTarget: boolean) => void;
     resetGlobalState: () => void;
 };
 
 export const globalStateContext = createContext<GlobalStateContext | null>(null);
 
-export function useGlobalState(): GlobalStateContext {
-    return useContext(globalStateContext) || { ...globalStateDefault, brand: 'anywhere' };
+export function useGlobalState(): GlobalStateContext & { theme: ColorTheme; setTheme: (theme: ColorTheme) => void } {
+    const globalState = useContext(globalStateContext);
+
+    const { theme, setTheme } = useUIContext();
+
+    return { ...(globalState || globalStateDefault), theme, setTheme };
 }
 
 /** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
