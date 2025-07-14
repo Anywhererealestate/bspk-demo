c8de570bce1d28e0475567af50dd4b9fb3c19317
anybrian <brian.reed@anywhere.re>
2025-07-14 15:20:48 -0400

    first pass

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,15 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, CSSProperties } from 'react';
 
-export function Page({ children }: PropsWithChildren) {
-    return <div data-page>{children}</div>;
+interface PageProps extends PropsWithChildren {
+    style?: CSSProperties;
+}
+
+export function Page({ children, style }: PageProps) {
+    return (
+        <div data-page style={style}>
+            {children}
+        </div>
+    );
 }
 
 /** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
