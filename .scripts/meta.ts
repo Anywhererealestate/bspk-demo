4424d8d467f8234b0d4a650331f14ff39beeb530
anybrian <brian.reed@anywhere.re>
2025-07-18 13:54:20 -0400

    deploy logging

--- .scripts/meta.ts
+++ .scripts/meta.ts
@@ -28,6 +28,9 @@ function runMetaCommand({ prefix, hash, updated }: { prefix: string; hash: strin
     } catch {
         // If the git command fails, we assume no new commits have been made
     }
+
+    console.log(`Building @bspk/ui meta with prefix: ${prefix}, hash: ${hash}, updated: ${updated}, build: ${build}`);
+
     execSync(`${prefix} npm run meta out=${outDir} hash=${hash} build=${build} ${updated ? `update=${updated}` : ''}`, {
         stdio: 'inherit',
     });
