c66b0dfcb4da4f0ff2a54aa3d69de9ddfc4c469c
anybrian <anybrian@users.noreply.github.com>
2025-10-09 18:24:53 +0000

    deploy: 8460bca43b139277d8f6b95e09e9565700a3116d

--- assets/index-BvFDE15Y.js
+++
@@ -1,9 +0,0 @@
-import{j as r,F as c}from"./index-BDnGPAyG.js";import{DateInput as m}from"./index-CJde5JYi.js";import"./index-CTAA0Sla.js";import"./KeyboardDoubleArrowLeft-C78uO6sf.js";import"./KeyboardDoubleArrowRight-CX1YbjPA.js";const i=document.createElement("style");i.appendChild(document.createTextNode(`[data-bspk=date-field] {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: fit-content;
-}
-
-/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
-`));document.head.appendChild(i);function j({label:e,errorMessage:a,helperText:d,invalid:t,labelTrailing:n,required:o,controlId:l,...p}){return r.jsx(c,{controlId:l,"data-bspk":"date-field",errorMessage:a,helperText:d,invalid:t,label:e,labelTrailing:n,required:o,children:s=>r.jsx(m,{...p,...s,"aria-label":e,invalid:t})})}export{j as DateField};
