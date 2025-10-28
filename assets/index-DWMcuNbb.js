92d52b243a760fc511c5f48128e6133a82dac7c1
anybrian <anybrian@users.noreply.github.com>
2025-10-28 21:11:50 +0000

    deploy: 5239b0ef24a68d7bfa47341a78498faad58909fe

---
+++ assets/index-DWMcuNbb.js
@@ -0,0 +1,12 @@
+import{j as p}from"./index-B0vhOQuU.js";const e=document.createElement("style");e.appendChild(document.createTextNode(`[data-bspk=chip-group] {
+  display: flex;
+  gap: var(--spacing-sizing-02);
+  width: 100%;
+  overflow: auto;
+}
+[data-bspk=chip-group][data-wrap] {
+  flex-flow: row wrap;
+}
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
+`));document.head.appendChild(e);function d({children:t,wrap:a=!0}){return p.jsx("div",{"data-bspk":"chip-group","data-wrap":a||void 0,children:t})}export{d as ChipGroup};
